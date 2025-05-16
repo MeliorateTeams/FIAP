38a1caccd03d4674a1ced2a4380be4b27d394b26
Tommyscripts <javier.cabrera.escoz@gmail.com>
2025-05-16 10:56:47 +0100

    Nuevo push

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import IndexPage from './views/IndexPage'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
+  <StrictMode> 
+    <IndexPage/>
   </StrictMode>,
 )
