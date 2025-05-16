46f7732179850553e875d2e6b657c7aa5a81ab85
Tommyscripts <javier.cabrera.escoz@gmail.com>
2025-05-16 16:02:49 +0100

    tailwind instalado

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,12 +2,13 @@
 
 export default function Header() {
   return (
-    <header className="bg-slate-800">
+    <header className="bg-gray-800 radius-2xl p-4 mb-4">
         <div className="flex justify-between items-center">
-            Esto es una prueba del navbar y header
+          <div>
+          Esto es una prueba del navbar y header
+          </div>
         </div>
         <nav>
-
         </nav>
     </header>
   )
--- src/views/IndexPage.tsx
+++ src/views/IndexPage.tsx
@@ -1,9 +1,11 @@
 import Header from "../components/Header";
 
-export default function IndexPage() {
+function IndexPage() {
   return (
     <>
      <Header />
     </>
   )
 }
+
+export default IndexPage;
