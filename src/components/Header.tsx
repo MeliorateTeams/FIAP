ff46b4f8ba37c6aaaed2dddc4c2ef43f0d96142f
Lara Mesa <laramesacubas@gmail.com>
2025-05-16 12:04:27 +0100

    cambios

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+
+
+export default function Header() {
+  return (
+    <header className="bg-slate-800">
+        <div className="flex justify-between items-center">
+            Esto es una prueba del navbar y header
+        </div>
+        <nav>
+
+        </nav>
+    </header>
+  )
+}
